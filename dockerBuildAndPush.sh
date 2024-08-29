351059820eb3610429aa4ff02b77b48064db2e1c
Samuel Oluwade <samueloluwade.devops@gmail.com>
2024-08-29 15:08:20 -0500

    minor change to force redeployment after docker image updated

--- dockerBuildAndPush.sh
+++ dockerBuildAndPush.sh
@@ -2,4 +2,4 @@
 docker build -t marstourist .
 docker tag marstourist:latest webappsandapis.azurecr.io/marstourist:latest
 az acr login --name webappsandapis
-docker push webappsandapis.azurecr.io/marstourist:latest
+docker push webappsandapis.azurecr.io/marstourist:latest 
